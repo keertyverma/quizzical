9b831d29336eddf5568e46d5164a17f5d9e99436
Keerty Verma <keertyverma1303@gmail.com>
2023-04-19 15:38:09 +0530

    Add game intro view

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import topShape from "./assets/images/top-shape.png";
+import bottomShape from "./assets/images/bottom-shape.png";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Quizzical</h1>
-    </div>
+    <main>
+      <img className="top-shape" src={topShape}></img>
+      <img className="bottom-shape" src={bottomShape}></img>
+
+      <section className="game-intro">
+        <h1 className="game-title">Quizzical</h1>
+        <p>Play, learn, and conquer</p>
+        <button className="btn-primary">Start Quiz</button>
+      </section>
+    </main>
   );
 }
 
